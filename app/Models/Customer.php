dacaf6c2bc4f5971989c7cf95eb3ac4c373d52f8
abrahamuchos <j.abraham29@gmail.com>
2024-05-17 15:38:03 -0400

    Add models migrations, controllers, request, resources

---
+++ app/Models/Customer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $type
+ * @property string $email
+ * @property string $address
+ * @property string $city
+ * @property string $state
+ * @property string $country
+ * @property string $postal_code
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Invoice> $invoices
+ * @property-read int|null $invoices_count
+ * @method static \Database\Factories\CustomerFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePostalCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Customer extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        ''
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function invoices(): HasMany
+    {
+     return $this->hasMany(Invoice::class);
+    }
+
+
+
+
+}
---
+++ app/Models/Invoice.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $customer_id
+ * @property int $invoice_number
+ * @property string $uuid
+ * @property float $amount
+ * @property string $status
+ * @property string $billed_dated
+ * @property string|null $paid_dated
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Customer|null $customer
+ * @method static \Database\Factories\InvoiceFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereBilledDated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereCustomerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereInvoiceNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice wherePaidDated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Invoice whereUuid($value)
+ * @mixin \Eloquent
+ */
+class Invoice extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      ''
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+
+}
